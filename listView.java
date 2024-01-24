5899edb95430c220bab7f527448a82660e9f5bfc
YRN <118609415+Kascald@users.noreply.github.com>
2024-01-24 15:53:40 +0900

    fixed listview

--- listView.java
+++ listView.java
@@ -5,19 +5,18 @@ import java.util.Collection;
 public class listView {
 
     private DAO dao = new DAO();
-
     public void listview() {
         Collection<productVO> productList = dao.selectList();
-
+        
+        
         System.out.println("상품 목록");
         System.out.println("--------------------------------------------------------");
         System.out.println("| 상품명 \t| 분류 \t| 가격 \t| 재고 \t|");
 
-        for (productVO product : productList) {
-            System.out.printf("| %s \t| %s \t| %s \t| %s \t|\n",
-                    product.getName(), product.getCateId(), product.getCst(), product.getCnt());
-        }
 
+        productList.stream()
+        .forEach(vo -> System.out.printf("| %s \t| %s \t| %s \t| %s \t|\n",
+        vo.getName() , vo.getCateId(), vo.getCst() , vo.getCnt()));
         System.out.println("--------------------------------------------------------");
 
 }
