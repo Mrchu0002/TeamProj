b14fb9124ec4a802afd28d5c8c1f6be424fc6054
Mrchu0002 <mrchu0002@gmail.com>
2024-01-25 12:39:35 +0900

    Update listView.java

--- listView.java
+++ listView.java
@@ -15,12 +15,12 @@ public class listView {
         
         System.out.println("상품 목록");
         System.out.println("--------------------------------------------------------");
-        System.out.printf("\"%-10s\" \"%8s\" \"%8s\" \"%8s\" \n" , name, category , cost, count);
+        System.out.printf("%s : [%s], %s, %s \n" , name, category , cost, count);
 
 
         productList.stream()
-        .forEach(vo -> System.out.printf("\"%-10s\" \"%8s\" \"%8s\" \"%8s\"\n",
-        vo.getName(), vo.getCateId(), vo.getCst(), vo.getCnt())); //여기 순서 바꾸고
+        .forEach(vo -> System.out.printf("%s : [%s], %s, %s \n",
+        vo.getCateId(), vo.getName(), vo.getCst(), vo.getCnt())); //여기 순서 바꾸고
         System.out.println("--------------------------------------------------------");
 
 }
