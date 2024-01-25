0d4adce6d88c0712b01afaca36677fa62ba851e3
Mrchu0002 <mrchu0002@gmail.com>
2024-01-25 10:57:21 +0900

    Update listView.java

--- listView.java
+++ listView.java
@@ -11,13 +11,17 @@ public class listView {
         
         System.out.println("상품 목록");
         System.out.println("--------------------------------------------------------");
-        System.out.println("| 상품명 \t| 분류 \t| 가격 \t| 재고 \t|");
+        System.out.println("| 상품명 | 분류 | 가격 | 재고 |");
 
 
         productList.stream()
-        .forEach(vo -> System.out.printf("| %s \t| %s \t| %s \t| %s \t|\n",
+        .forEach(vo -> System.out.printf("| %s | %s | %s | %s |\n",
         vo.getName() , vo.getCateId(), vo.getCst() , vo.getCnt()));
         System.out.println("--------------------------------------------------------");
 
+        //add 라면0 RM101 1000 100
+        //add 라면1 RM102 2000 200
+        //add 라면2 RM103 3000 300
+    
 }
 }
